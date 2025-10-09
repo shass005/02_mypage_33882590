778682156a53302bfbb1257b44f345cd4d38b419
shass005 <safwanhassan013@gmail.com>
2025-10-09 11:25:23 +0100

    Initial Version

---
+++ index.js
@@ -0,0 +1,35 @@
+var http = require("http"); 
+const port = 8000; 
+
+http.createServer(function(req, res) { 
+    console.log(req);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+        <!DOCTYPE html>
+        <html>
+         <title>MyPage</title>
+         <style>
+          body{
+           background-color: black;
+          }
+          h1{
+           color: white;
+          }
+          h2{
+           color: white;
+          }
+          p{
+           color: white;
+          }   
+         </style>
+        </html>
+        <body>
+         <h1>This is MyPage</h1>
+         <h2>About Me:</h2>
+         <p>I am an Undergraduate Computer Science student at Goldsmith University. This is my third and final year at Goldsmith...o</p>
+        </body>
+        
+        `);
+    }).listen(port, function() { 
+        console.log(`Node server is running on port ${port}...`); 
+}); 
